2b52ceb64ccbe65e662a6969a9d66d681be0c801
xshirl <xshirl525@gmail.com>
2021-01-03 20:07:43 -0500

    added controllers and routes

--- controllers/index.js
+++ controllers/index.js
@@ -34,7 +34,100 @@ const userOfRequest = (req) => {
 }
 
 //AUTH
+const signUp = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { username, name, password, admin_key } = req.body;
+    const password_digest = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = await new User({
+      username,
+      name,
+      password_digest,
+    });
+    await user.save();
 
+    const payload = {
+      id: user._id,
+      username: user.username,
+      name: user.name,
+    };
+
+    const token = jwt.sign(payload, TOKEN_KEY);
+    return res.status(201).json({ user: payload, token });
+  } catch (error) {
+    console.log("Error in signUp");
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+const signIn = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username: username });
+    if (await bcrypt.compare(password, user.password_digest)) {
+      const payload = {
+        id: user._id,
+        username: user.username,
+        name: user.name,
+      };
+      const token = jwt.sign(payload, TOKEN_KEY);
+      return res.status(201).json({ user: payload, token });
+    } else {
+      res.status(401).send("Invalid Credentials");
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+//verify user
+const verifyUser = async (req, res) => {
+  try {
+    // can only verify with JWT
+    const legit = await userOfRequest(req);
+
+    if (legit) {
+      const user = await User.findById(legit.id);
+
+      const profile = {
+        id: user._id,
+        username: user.username,
+        name: user.name,
+      };
+
+      return res.status(200).json({ user: profile });
+    }
+    return res.status(401).send("Not Authorized");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getCategories = async (req, res) =>  {
+  try {
+    const categories = await Category.find()
+    return res.status(200).json(categories);
+  } catch(error) {
+    return res.status(500).json({error: error.message})
+  }
+}
+
+const getFlashcards = async(req, res) => {
+  try {
+    const legit = await userOfRequest(req);
+    if(legit) {
+      const categoryId = req.params.id;
+      const flashcards = await Flashcard.find(
+        {"category": categoryId}
+      );
+      return res.status(200).json(categories);
+    }
+    return res.status(401).send("Not Authorized");
+
+  } catch(error) {
+    return res.status(500).json({error: error.message})
+  }
+}
 
 
 module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const { Router } = require('express');
 const router = Router();
-const controllers = require('../controllers');
+const controller = require('../controllers');
 
 router.get('/', (req, res) => res.send("This is root"))
+
+//users
+router.post("/signup", (req, res) => controller.signUp(req,res));
+router.post("/signin", (req, res) => controller.signIn(req, res));
+router.post("/verifyuser", (req, res) => controller.verifyUser(req, res));
+router.get("/categories/:id", (req, res) => controller.getFlashcards(req, res));
+router.get('/categories', (req, res) => controller.getCategories(req, res));
+
 
 module.exports = router
